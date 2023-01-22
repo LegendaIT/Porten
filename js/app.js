f51f16d6186e597fc4f148f1cadf15616234ae37
LegendaIT <103688490+LegendaIT@users.noreply.github.com>
2023-01-22 23:37:08 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,30 @@
+(() => {
+    "use strict";
+    function isWebp() {
+        function testWebP(callback) {
+            let webP = new Image;
+            webP.onload = webP.onerror = function() {
+                callback(2 == webP.height);
+            };
+            webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+        }
+        testWebP((function(support) {
+            let className = true === support ? "webp" : "no-webp";
+            document.documentElement.classList.add(className);
+        }));
+    }
+    let addWindowScrollEvent = false;
+    setTimeout((() => {
+        if (addWindowScrollEvent) {
+            let windowScroll = new Event("windowScroll");
+            window.addEventListener("scroll", (function(e) {
+                document.dispatchEvent(windowScroll);
+            }));
+        }
+    }), 0);
+    const search = document.querySelector("[type = 'search']");
+    const searchImg = document.querySelector(".form__search > img");
+    searchImg.onclick = () => search.classList.toggle("active");
+    window["FLS"] = true;
+    isWebp();
+})();
